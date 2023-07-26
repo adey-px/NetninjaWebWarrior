8ec9e2a0aa171056142e6228d5c56d360eeec273
adey-px <aosteph@gmail.com>
2023-07-25 19:11:27 -0500

    Setup new gatsby project

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export default function Home() {
+	return (
+		<section>
+			<div>
+				<h2>Design</h2>
+				<h3>Build & Deploy</h3>
+				<p>UX Designer & Web Developer based in Texas</p>
+			</div>
+		</section>
+	);
+}
